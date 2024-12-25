d425bf403383db1eff0db5efb50b60dad96904d9
Hamza Rabah <h.rabah@outlook.com>
2024-12-25 13:19:58 +0300

    add docker support for local dev

--- Ba7besh.Api/Program.cs
+++ Ba7besh.Api/Program.cs
@@ -58,8 +58,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 
 app.MapControllers();
 app.MapGet("/", () => "Hello World! v3");
